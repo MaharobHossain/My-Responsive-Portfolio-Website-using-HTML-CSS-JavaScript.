6a940183a165db0b5b08fbbb2f0e2e852595bf97
MaharobHossain <maharobhossain8880@gmail.com>
2024-03-12 15:50:23 +0600

    javascript work

--- main.js
+++ main.js
@@ -53,3 +53,7 @@ ScrollReveal({
     duration: 2000,
     delay: 200,
 });
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact from', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
