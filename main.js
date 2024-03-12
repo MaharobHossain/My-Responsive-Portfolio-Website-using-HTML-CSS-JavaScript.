e1e23a96b5107e068e5f0324afd70571560a60b4
MaharobHossain <maharobhossain8880@gmail.com>
2024-03-12 16:29:25 +0600

    java all work done

--- main.js
+++ main.js
@@ -57,3 +57,13 @@ ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact from', { origin: 'buttom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*==========  typed js  ========*/
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Developer', 'Web Designer', 'Influencer' ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
